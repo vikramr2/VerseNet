d046408434bf6e0fdde5c442f78bf192c1fb0258
Vikram Ramavarapu <54862162+vikramr2@users.noreply.github.com>
2020-11-27 15:28:09 -0600

    Style and comments

--- rnn/model.py
+++ rnn/model.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
 
+'''
+An RNN feeds into an embedding to encode for an RNN unit to process, then a linear layer to decode into a next word in generation
+'''
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, model_type="gru", n_layers=1):
         super(RNN, self).__init__()
@@ -18,22 +21,18 @@ class RNN(nn.Module):
         elif self.model == "lstm":
             self.rnn = nn.LSTM(hidden_size, hidden_size, n_layers)
         self.decoder = nn.Linear(hidden_size, output_size)
-        #self.bn = nn.BatchNorm1d(output_size)
 
     def forward(self, input, hidden):
         batch_size = input.size(0)
         encoded = self.encoder(input)
         output, hidden = self.rnn(encoded.view(1, batch_size, -1), hidden)
         output = self.decoder(output.view(batch_size, -1))
-        #output = self.bn(output)
         return output, hidden
-
-    def forward2(self, input, hidden):
-        encoded = self.encoder(input.view(1, -1))
-        output, hidden = self.rnn(encoded.view(1, 1, -1), hidden)
-        output = self.decoder(output.view(1, -1))
-        return output, hidden
-
+    
+    '''
+    The forward pass requires a hidden matrix as a parameter to learn when generating text.
+    We start by initializing this matrix to zeros
+    '''
     def init_hidden(self, batch_size, device):
         if self.model == "lstm":
             return (Variable(torch.zeros(self.n_layers, batch_size, self.hidden_size)).to(device),
